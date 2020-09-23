bc14ddcd55bfa63f19309e0546b7e966d5cf25c1
Murray Smith <murraysmith2727@gmail.com>
2020-09-23 10:52:51 -0400

    Committed all files so far

---
+++ Assets/Scripts/GrapplingScript.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingScript : MonoBehaviour
+{
+
+    private LineRenderer lr;
+    private Vector3 GrapplingTo;
+    public LayerMask whatIsGrapplable;
+    public Transform grapplePointOnPlayer;
+    private SpringJoint joint;
+    public float maxDistanceMultiplierForJoint = 0.8f;
+    public float minDistanceMultiplierForJoint = 0.25f;
+    public float JointSpring = 4.5f;
+    public float JointDamper = 7f;
+    public float JointMassScale = 4.5f; 
+    public float DeleteJointThreshold = 0.6f;
+    float DistanceToPoint;
+    bool DestroyJoint;
+    bool LeftClick;
+    bool RightClick;
+    bool CanLeftClick = true;
+    bool CanRightClick = true;
+    void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    void Update(){
+        DestroyJoint = DistanceToPoint < DeleteJointThreshold;
+        if (CanLeftClick){
+            LeftClick = Input.GetMouseButtonDown(0);
+        }
+        if (CanRightClick){
+            RightClick = Input.GetMouseButton(1);
+        }
+    }
+    void FixedUpdate()
+    {
+        DistanceToPoint = Vector3.Distance(transform.position, GrapplingTo);
+        if (LeftClick){
+            GrappleToPoint();
+            CanLeftClick = false;
+            LeftClick = false;
+        }
+        else if (RightClick){
+            PullObject();
+            CanRightClick = false;
+            RightClick = false;
+        }
+        if (DestroyJoint){
+            Destroy(joint);
+            lr.positionCount = 0;
+            CanLeftClick = true;
+        }
+    }
+
+    void LateUpdate(){
+        DrawRope();
+    }
+
+
+    void GrappleToPoint(){
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, maxDistance: Mathf.Infinity, layerMask: 1<<8)){
+            GrapplingTo = hit.point;
+            joint = transform.gameObject.AddComponent<SpringJoint>();
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedAnchor = GrapplingTo;
+
+            joint.maxDistance = DistanceToPoint * maxDistanceMultiplierForJoint;
+            joint.minDistance = DistanceToPoint * minDistanceMultiplierForJoint;
+
+            joint.spring = JointSpring;
+            joint.damper = JointDamper;
+            joint.massScale = JointMassScale;
+        }
+    }
+
+    void DrawRope(){
+
+        if (!joint) return;
+        lr.positionCount = 2;
+        lr.SetPosition(0, grapplePointOnPlayer.position);
+        lr.SetPosition(1, GrapplingTo);
+    }
+    void DeleteRope(){
+        lr.positionCount = 0;
+    }
+
+    void PullObject(){
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //TODO: Make it so this is physics based 
+    Vector3 MoveVector = new Vector3(0, 0, 0);
+    public float xvel = 0;
+    public float zvel = 0;
+    public float acc = 0.001f;
+    public float speedlimit = 0.5f;
+    public float InitialSpeed = 0.1f;
+    public float JumpBufferTime = 0.1f;
+    public float jumpForce = 0;
+    public float MidAirDrift = 0.5f;
+    bool IsGrounded;
+    bool WasGrounded;
+    float TimeOfLastLanding = 0;
+
+    float TimeSinceLastLanding;
+    float jump;
+    bool SpaceDown;
+    bool DDown;
+    bool ADown;
+
+    CapsuleCollider collider;
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<CapsuleCollider>();
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update(){
+        SpaceDown = Input.GetKey(KeyCode.Space);
+        ADown = Input.GetKey(KeyCode.A);
+        DDown = Input.GetKey(KeyCode.D);
+    }
+
+    void FixedUpdate()
+    {
+        
+        HandleKeyBoardMouseInputs();
+        
+    }
+
+
+    void HandleKeyBoardMouseInputs(){
+        jump = 0;
+        WasGrounded = IsGrounded;
+        IsGrounded = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), transform.localScale.y / 2.0f + 0.0001f);
+        if (ADown){
+            if (zvel == 0 && IsGrounded){
+                zvel = -InitialSpeed;
+            }else if (zvel == 0 && !IsGrounded){
+                zvel -= InitialSpeed * 0.02f;
+            } else if (zvel > - speedlimit + acc && IsGrounded){
+                zvel -= acc;
+            }else if (!IsGrounded){
+                zvel -= MidAirDrift * acc;
+            }
+        }else if(zvel < 0) {
+            zvel = 0;
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, 0);
+        }
+        
+        if (DDown){
+            if (zvel == 0 && IsGrounded){
+                zvel = InitialSpeed;
+            } else if (zvel == 0 && !IsGrounded){
+                zvel += InitialSpeed * 0.02f;
+            }else if (zvel < speedlimit - acc && IsGrounded){
+                zvel += acc;
+            } else if (!IsGrounded){
+                zvel += MidAirDrift * acc;
+            }
+        } else if (zvel > 0){
+            zvel = 0;
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, 0);
+        }
+      
+        if (SpaceDown){
+            
+            if (!WasGrounded && IsGrounded){
+                TimeOfLastLanding = Time.time;
+            }
+            
+            TimeSinceLastLanding = Time.time - TimeOfLastLanding;
+            if (IsGrounded && TimeSinceLastLanding > JumpBufferTime){
+                jump = 1.0f;
+                
+            }
+        }
+
+        rigidBody.AddForce(new Vector3(0, jump * jumpForce, 0), ForceMode.Impulse);
+        
+        MoveVector.z = zvel;
+        if (MoveVector.z != 0){
+            rigidBody.MovePosition(transform.position + MoveVector * Time.deltaTime);
+        }
+    }
+}
