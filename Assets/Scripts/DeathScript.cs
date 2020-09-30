ea8297c856065262ae86310a9b2eb9ec0ba2a086
tonyhu-x <807769469hjt@gmail.com>
2020-09-30 10:03:19 -0400

    Create DeathScript

---
+++ Assets/Scripts/DeathScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScript : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col) {
+        if(col.collider.CompareTag("Player")) {
+            Debug.Log("I'm dead!");
+        }
+    }
+}
